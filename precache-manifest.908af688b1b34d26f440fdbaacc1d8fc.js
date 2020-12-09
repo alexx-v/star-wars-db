3f0cff809dae4c6101979f81354fb04c2234ea6c
alexx-v <aleksandr1vlk@gmail.com>
2020-12-09 15:02:20 +0200

    Updates

---
+++ precache-manifest.908af688b1b34d26f440fdbaacc1d8fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56aadd4c8b1f2ef2c157c57ee843f4e",
+    "url": "/star-wars-db/index.html"
+  },
+  {
+    "revision": "65e80853c17dde2b2e3c",
+    "url": "/star-wars-db/static/css/main.c5b8b370.chunk.css"
+  },
+  {
+    "revision": "85765a0320666c0001e0",
+    "url": "/star-wars-db/static/js/2.b974992c.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/star-wars-db/static/js/2.b974992c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e80853c17dde2b2e3c",
+    "url": "/star-wars-db/static/js/main.08fd8b1f.chunk.js"
+  },
+  {
+    "revision": "c88d8dbc373f13a940e0",
+    "url": "/star-wars-db/static/js/runtime-main.278e2f2d.js"
+  },
+  {
+    "revision": "02b971fe4719034bc990aeb2f16e62b6",
+    "url": "/star-wars-db/static/media/boom.02b971fe.gif"
+  }
+]);
