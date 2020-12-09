8faeeacfad9cd377931b2b295762c094488eace7
alexx-v <aleksandr1vlk@gmail.com>
2020-12-09 15:07:15 +0200

    adding files for github pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,7 @@ const App = () => {
 	return (
 		<ErrBoundary>
 			<Container fluid='lg' className='app'>
-				<Router>
+				<Router basename='/star-wars-db/'>
 					<Header />
 
 					<RandomPlanet />
